55ad1a20f7ff39d6cb971b09ff928b594badf592
jcarnes1 <77592201+jcarnes1@users.noreply.github.com>
2021-03-15 00:35:06 -0400

    Delete Mod9.R

--- Mod9.R
+++
@@ -1,6 +0,0 @@
-weightLoss <- read.csv('WeightLoss.csv', stringsAsFactors = FALSE)
-barplot(wl1 ~ X, data = weightLoss)
-barplot(wl2 ~ X, data = weightLoss)
-barplot(wl3 ~ X, data = weightLoss)
-data <- structure(list(w1 = c(weightLoss$wl1), w2 = c(weightLoss$wl2), w3 = c(weightLoss$wl3)), .Names = c("Month 1", "Month 2", "Month 3"), class = "data.frame", row.names = weightLoss$X)
-barplot(as.matrix(data), main = "Weight Loss", ylab = "Weight Lost (lbs)", cex.lab = 1.5, cex.main = 1.4, beside = TRUE)
